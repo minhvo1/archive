d49e72a65e135027cc05a9d81ea866cff9e9362c
Minh <minhvo199@gmail.com>
2022-12-24 00:10:29 -0700

    add sketch p5js

--- src/components/InfoDisplay.vue
+++ src/components/InfoDisplay.vue
@@ -1,11 +1,16 @@
 <template>
   <div class=" flex-auto bg-black">
-    
+    <GenCanvas/>
   </div>
 </template>
 
 <script>
+import GenCanvas from './GenCanvas.vue'
+
 export default {
   name: 'InfoDisplay',
+  components: {
+    GenCanvas
+  }
 }
 </script>
