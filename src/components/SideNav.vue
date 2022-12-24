347b2662d540d6947effacf1ee67631ff6469071
Minh <minhvo199@gmail.com>
2022-12-23 21:58:11 -0700

    add sidenav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex-auto w-full max-w-xl">
-    <div class="header">
+  <div class="side-nav flex-auto w-full max-w-xl pl-3">
+    <div class="header text-8xl font-bold mb-9">
       <h2>GEN</h2>
     </div>
 
-    <div class="menu">
-      
+    <div class="menu text-7xl mt-16">
+      <div class="menu-item flow-field">
+        <p>FLOW FIELD</p>
+      </div>
     </div>
   </div>
 </template>
@@ -16,6 +18,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
   @import '../styles/sideNav.scss';
 </style>
