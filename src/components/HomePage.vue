59b63c41c763df59da0d1778138dd5713f3f68d0
Minh <minhvo199@gmail.com>
2022-12-28 23:21:47 -0700

    add home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,13 @@
 <script setup>
+
 </script>
 
 <template>
-  <div>Home</div>
+  <div class="home text-right pr-7 pl-48 h-screen flex flex-col justify-end align-bottom">
+    <p class="home-text">A COLLECTION OF GENERATIVE ART SKETCHES AND ALGORITHMS.</p>
+  </div>
 </template>
+
+<style lang="scss">
+@import '../styles/home.scss';
+</style>
